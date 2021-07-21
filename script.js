51b5c52db9554eb1ce84cf0d5522d690a8234a1d
Fernetazo <angelofdarkness88@gmail.com>
2021-07-21 18:48:46 -0300

    UI, new js code

--- script.js
+++ script.js
@@ -1,57 +1,63 @@
 let cpuOptions = ['rock', 'paper', 'scissors'];
+let win = 0;
+let lose = 0;
+currentResult = document.getElementById("currentResult");
+roundResult = document.getElementById("roundResult");
+finalResult = document.getElementById("finalResult");
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', playRound));
 
 function computerPlay(max){
     return Math.floor(Math.random() * max);
 }
 
 function playRound(playerSelection, computerSelection){
+
+    playerSelection = playerSelection.target.id;
+    computerSelection = cpuOptions[computerPlay(3)];
+
+    if (win === 0 && lose === 0){
+        finalResult.textContent = "";
+    }
     if (playerSelection == computerSelection){
-        return ("It's a tie!");
+        roundResult.textContent = "It's a tie!";
     }
     if (playerSelection == 'rock' & computerSelection == 'paper'){
-        return ("You Lose! Paper beats Rock");
+        roundResult.textContent = "You Lose! Paper beats Rock";
+        lose++;
     }
     if (playerSelection == 'paper' & computerSelection == 'scissors'){
-        return ("You Lose! Scissors beats Paper");
+        roundResult.textContent = "You Lose! Scissors beats Paper";
+        lose++;
     }
     if (playerSelection == 'scissors' & computerSelection == 'rock'){
-        return ("You Lose! Rock beats Scissors");
+        roundResult.textContent = "You Lose! Rock beats Scissors";
+        lose++;
     }
     if (playerSelection == 'rock' & computerSelection == 'scissors'){
-        return ("You Win! Rock beats Scissors");
+        roundResult.textContent = "You Win! Rock beats Scissors";
+        win++;
     }
     if (playerSelection == 'paper' & computerSelection == 'rock'){
-        return ("You Win! Paper beats Rock");
+        roundResult.textContent = "You Win! Paper beats Rock";
+        win++;
     }
     if (playerSelection == 'scissors' & computerSelection == 'paper'){
-        return ("You Win! Scissors beats Paper");
+        roundResult.textContent = "You Win! Scissors beats Paper";
+        win++;
+    }
+    
+    currentResult.textContent = `You ${win} - ${lose} CPU`;
+
+    if (win === 5){
+        finalResult.textContent = "You WIN the match!!! :)";
+        win = 0;
+        lose = 0;
+    }
+    else if (lose === 5){
+        finalResult.textContent = "You LOSE the match... :(";
+        win = 0;
+        lose = 0;
     }
 }
-
-function Game (){
-    let win = 0;
-    let lose = 0;
-    for (let i = 0; i < 5; i++){
-        const playerSelection = prompt("Input your play").toLowerCase();
-        const computerSelection = cpuOptions[computerPlay(3)];
-        console.log(result = playRound(playerSelection, computerSelection));
-        if (result.includes("Win")){
-            win++;
-        }
-        if (result.includes("Lose")){
-            lose++;
-        }
-    }
-    if (win > lose){
-        console.log("You WIN the match!!! :)");
-    }
-    else if (win < lose){
-        console.log("You LOSE the match... :(");
-    }
-    else{
-        console.log("TIEEE!");
-    }
-}
-
-console.log("Welcome!");
-Game();
